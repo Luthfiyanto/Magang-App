d22d9ed049685293c0801dddaec0a55ff6dd3d50
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2023-12-03 13:49:35 +0700

    fix: minor styling

--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -2,7 +2,7 @@ function Jumbotron() {
   return (
     <header className="font-sans bg-[url('./assets/background.jpeg')] bg-fixed w-full h-80 flex justify-center items-center text-center">
       <div>
-        <h1 className="font-bold text-slate-800 p-2 text-lg lg:text-3xl">MITRA MAGANG MSIB BATCH 6</h1>
+        <h1 className="font-bold text-violet-200 md:text-slate-800 p-2 text-lg lg:text-3xl">MITRA MAGANG MSIB BATCH 6</h1>
         <p className="font-medium text-slate-100 shadow-sm shadow-white p-3">Cari tahu kuota magang pada mitra pilihanmu</p>
       </div>
     </header>
